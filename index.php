ab5b037f34dee9c960440d6814a1ac98ea8f1687
Dave Palica <30877122+dwp32@users.noreply.github.com>
2020-01-07 10:27:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 #  phpinfo();
 #  die;
+#var_dump($_POST);
 
-$color = substr(str_shuffle('ABCDEF0123456789'), 0, 6);
-$colors[$color] = '#' . $color;
-
+if(isset($_POST['color']))
+{
+  $color = $_POST['color'];
+}
+else
+{
+  echo $color;
+  echo "Color is null<br>";
+  $color = substr(str_shuffle('ABCDEF0123456789'), 0, 6);
+  echo "New color is: $color";
+  echo "<form action='' method='POST'>";
+  echo "   <input type='hidden' name='color' value='$color'>";
+  echo "   <input type='submit'>";
+  echo "</form>";
+}
+echo $color;
 echo "<!DOCTYPE html>";
 echo "<html>";
 echo "<head>";
