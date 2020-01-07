fbff9bbdd350193b89c32fe86f1aad5d92595a62
Dave Palica <30877122+dwp32@users.noreply.github.com>
2020-01-07 09:31:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
 <?php
-$rgbColor = array();
- 
-//Create a loop.
-foreach(array('r', 'g', 'b') as $color){
-    //Generate a random number between 0 and 255.
-    $rgbColor[$color] = mt_rand(0, 255);
-}
- 
-var_dump($rgbColor);
-
-
-<div style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
-    Random Color!
-</div>
-1
-2
-3
-<div style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
-    Random Color!
-</div>
+phpinfo();
 ?>
