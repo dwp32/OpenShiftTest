797554cddf83c4f65d399793b370941e9fc5baf2
Dave Palica <30877122+dwp32@users.noreply.github.com>
2020-01-07 09:29:22 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
-phpinfo();
-echo "<!DOCTYPE html>";
-echo "<html>";
-echo "<head>";
-echo "  <title>JavaScript function to create random background color</title>";
-echo "</head>";
-echo "<body>";
-echo "</body>";
-echo "</html>";
+$rgbColor = array();
+ 
+//Create a loop.
+foreach(array('r', 'g', 'b') as $color){
+    //Generate a random number between 0 and 255.
+    $rgbColor[$color] = mt_rand(0, 255);
+}
+ 
+var_dump($rgbColor);
+
+
+<div style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
+    Random Color!
+</div>
+1
+2
+3
+<div style="background-color: rgb(<?= implode(",", $rgbColor); ?>);">
+    Random Color!
+</div>
 ?>
