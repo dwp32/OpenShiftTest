eea064094a86fb29c71c5412989bbb19015c25bc
Dave Palica <30877122+dwp32@users.noreply.github.com>
2020-01-07 09:25:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ phpinfo();
 echo "<!DOCTYPE html>";
 echo "<html>";
 echo "<head>";
-echo "  <meta charset="utf-8">";
+echo "  <meta charset='utf-8'>";
 echo "  <title>JavaScript function to create random background color</title>";
 echo "</head>";
 echo "<body>";
